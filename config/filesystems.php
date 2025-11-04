83c464d973db58dd925a871f2448310eb59c2937
ThonyMarckDEV <thonymarck385213xd@gmail.com>
2025-11-03 21:53:30 -0500

    old

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,7 +56,7 @@ return [
             'bucket' => env('MINIO_BUCKET'),
             'endpoint' => env('MINIO_ENDPOINT'),
             'use_path_style_endpoint' => env('MINIO_USE_PATH_STYLE_ENDPOINT', true),
-            'throw' => true,
+            'throw' => false,
             'url' => env('MINIO_URL'), 
         ],
 
